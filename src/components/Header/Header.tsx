f1e1bc10d62249f3c77f7dad64937e7d8a6a2048
jalxby <jalxby@gmail.com>
2023-02-09 15:46:51 +0100

    commit 2023-02-09

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import s from "./Header.module.css"
+
+const Header = () => {
+    return (
+        <header className={s.header}>
+
+                <img className={s.headerImg}
+                    src={"https://www.imgacademy.com/sites/default/files/2020-img-cup-boys-invite-logo.png"} alt={"logo"}/>
+
+        </header>
+    );
+};
+
+export default Header;
